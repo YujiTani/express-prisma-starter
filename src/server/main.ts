7ec293cdb5376495e5b4a4042aea0b943194f3b7
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-02-01 00:41:17 +0900

    prismaを追加

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import ViteExpress from "vite-express";
 
+import { apiRouter } from "@/server/routes/index.ts";
+
 const app = express();
 
-app.get("/hello", (_, res) => {
-	res.send("Hello Vite + React + TypeScript!");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api", apiRouter);
+
+// 404エラー
+app.use((_req, res) => {
+	return res.status(404).json({ message: "Invalid API route" });
 });
 
-ViteExpress.listen(app, 3000, () => console.log("Server is listening on port 3000..."));
-
-console.log("Server is running on port 3000");
+ViteExpress.listen(app, 3005, () => console.log("Server is listening on port 3005..."));
