1088e920ad004e15c5294ce8579a8e0d30ec1160
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-02-01 00:57:21 +0900

    環境変数PORTを追加

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,9 +1,13 @@
+import dotenv from "dotenv";
 import express from "express";
 import ViteExpress from "vite-express";
 
 import { apiRouter } from "@/server/routes/index.ts";
 
+dotenv.config();
+
 const app = express();
+const port = process.env.PORT || 3005;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,4 +19,4 @@ app.use((_req, res) => {
 	return res.status(404).json({ message: "Invalid API route" });
 });
 
-ViteExpress.listen(app, 3005, () => console.log("Server is listening on port 3005..."));
+ViteExpress.listen(app, Number(port), () => console.log(`Server is listening on port ${port}...`));
